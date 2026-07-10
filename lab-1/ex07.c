6beb94f0dee6a20725f67b2f58a5e4e809fc7f7c
Isthatakanny <69011400@kmitl.ac.th>
2026-07-10 04:42:52 +0000

    removed an extra new line command

--- lab-1/ex07.c
+++ lab-1/ex07.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("\n\n %4s %9s","^", "^");
+    printf("\n %4s %9s","^", "^");
     printf("\n %s %3s %7s %5s", "(",".",".",")");
     printf("\n%10s\n","-");
 }
