84413aa8eca41de0d936ee7621bd5c6329cb2bd2
Isthatakanny <69011400@kmitl.ac.th>
2026-07-24 02:15:15 +0000

    for week 3

---
+++ quiz/quiz1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("\n%-14s %-4s %s","Programming",":","A");
+    printf("\n%-14s %-4s %s","Drawing",":","B+");
+    printf("\n%-14s %-4s %s","Mechanics",":","B+");
+    printf("\n%-14s %-4s %s","Math",":","B");
+    printf("\n%-14s %-4s %.2f\n","GPS",":",3.5);
+    return 0;
+}
